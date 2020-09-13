cceae9f779f66b2eebd1da9464cb1808dee43d35
Deyvison Ramos <deyvisonr0@gmail.com>
2020-09-13 20:51:47 -0300

    feat: unit tests the send message operation for errors

--- src/FinChat.Chat.UnitTests/ChatServiceTests.cs
+++ src/FinChat.Chat.UnitTests/ChatServiceTests.cs
@@ -1,8 +1,11 @@
 ﻿using System.Linq;
 using System.Threading.Tasks;
-using FinChat.Chat.Application.Enums;
+using FinChat.Chat.Application.Interfaces;
 using FinChat.Chat.Application.Services;
+using FinChat.Chat.Data.Transactions;
+using FinChat.Chat.Domain.Entities;
 using FinChat.Chat.Domain.Interfaces.Repositories;
+using FinChat.Chat.Domain.Notification;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 
@@ -12,12 +15,20 @@ namespace FinChat.Chat.UnitTests
     public class ChatServiceTests
     {
         private ChatService _chatService;
+        private Mock<IChatRoomRepository> _chatRoomMockRepository;
+        private Mock<IWebSocketService> _webSocketMock;
 
         [TestInitialize]
         public void Initialize()
         {
-            var chatRoomMockRepository = new Mock<IChatRoomRepository>();
-            _chatService = new ChatService(chatRoomMockRepository.Object);
+            _chatRoomMockRepository = new Mock<IChatRoomRepository>();
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+            _webSocketMock = new Mock<IWebSocketService>();
+
+            _chatService = new ChatService(
+                unitOfWorkMock.Object,
+                _chatRoomMockRepository.Object,
+                _webSocketMock.Object);
         }
 
         [DataTestMethod]
@@ -25,9 +36,14 @@ namespace FinChat.Chat.UnitTests
         [DataRow(null)]
         public async Task ShouldNotSendEmptyOrNullMessages(string message)
         {
+            _chatRoomMockRepository
+                .Setup(x =>
+                    x.GetChatRoomById(It.IsAny<string>(), It.IsAny<bool>()))
+                .Returns(Task.FromResult(new ChatRoom("test chat room")));
+
             var result = await _chatService.SendMessage("fakeChatId", "fakeAuthorId", "Fake Author", message);
             Assert.IsFalse(result.Success);
-            Assert.IsTrue(result.Messages.Any(x => x.Type == EOutputNotificationType.Error && x.Subject == "message"));
+            Assert.IsTrue(result.Notifications.Any(x => x.Type == ENotificationType.Error));
         }
     }
 }
