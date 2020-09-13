a6660b7aac8a31df193a3b3e393de996fb790599
Deyvison Ramos <deyvisonr0@gmail.com>
2020-09-13 02:30:52 -0300

    feat: structure setup

---
+++ src/FinChat.ChatBots.StockQuotation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FinChat.ChatBots.StockQuotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
